b3f78f38fdcf5e59ca9fadbb902947e0e69e60ca
Sushmita <sushmipalikhe97@gmail.com>
2021-07-27 14:26:59 +0545

    Creating tables [Invoice, InvoiceItem and Item]

---
+++ database/migrations/2021_07_27_081214_create_invoices_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateInvoicesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('invoices', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('company_id')
+                  ->constrained('companies')
+                  ->onUpdate('cascade')
+                  ->onDelete('cascade');
+            $table->foreignId('invoice_item_id')
+                  ->constrained('invoice_items')
+                  ->onUpdate('cascade')
+                  ->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('invoices');
+    }
+}
